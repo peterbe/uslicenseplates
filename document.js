ae25ee36faecfa964ae5b9b784c3480d994fd29f
Peter Bengtsson <mail@peterbe.com>
2012-07-10 18:01:18 -0700

    progress on timeSince()

--- document.js
+++ document.js
@@ -26,9 +26,49 @@ var State = (function() {  // no pun intended
     age: function(name) {
       if (typeof _state[name] !== 'number') return 60 * 60 * 24 * 4;  // legacy
       return ((new Date()).getTime() - _state[name]) / 1000;
+    },
+    oldest: function() {
+      var o = null;
+      $.each(_state, function(name, ts) {
+        if (typeof ts !== 'number') {
+          //ts = 60 * 60 * 24 * 4 * 1000;  // legacy
+          ts = Date.parse(ts);
+        }
+        if (o == null) o = ts;
+        else if (ts < o) o = ts;
+      });
+      return o;
     }
   }
 })();
+
+function timeSince(date) {
+  var delta = Math.round((new Date().getTime() - date) / 1000);
+
+  var minute = 60,
+    hour = minute * 60,
+    day = hour * 24,
+    week = day * 7;
+
+  var fuzzy;
+
+  if (delta < 30) {
+    fuzzy = 'just now';
+  } else if (delta < minute) {
+    fuzzy = delta + ' seconds ago';
+  } else if (delta < 2 * minute) {
+    fuzzy = 'a minute ago'
+  } else if (delta < hour) {
+    fuzzy = Math.floor(delta / minute) + ' minutes ago';
+  } else if (Math.floor(delta / hour) == 1) {
+    fuzzy = '1 hour ago'
+  } else if (delta < day) {
+    fuzzy = Math.floor(delta / hour) + ' hours ago';
+  } else if (delta < day * 2) {
+    fuzzy = 'yesterday';
+  }
+  return fuzzy;
+}
 
 
 var StatesForm = (function() {
@@ -41,6 +81,14 @@ var StatesForm = (function() {
     });
     $('#no-spotted', container).text(c);
     $('#no-remaining', container).text(uc);
+    var oldest = State.oldest();
+    if (oldest) {
+      $('#times-ago-outer').show();
+      $('#times-ago').text(timeSince(oldest));
+    } else {
+      $('#times-ago-outer').hide();
+    }
+
   }
   return {
      preload: function() {
